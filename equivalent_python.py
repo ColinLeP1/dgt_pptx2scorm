2b5b29930f712e6fd68a60c1c2e7048e09133060
ColinLeP1 <154586650+ColinLeP1@users.noreply.github.com>
2025-05-23 13:39:42 +0200

    titre

--- equivalent_python.py
+++ equivalent_python.py
@@ -12,8 +12,8 @@ uploaded_file = st.file_uploader("Téléversez un fichier PDF", type="pdf")
 
 # Définir le titre et le nom du fichier par défaut
 default_title = uploaded_file.name.replace(".pdf", "") if uploaded_file else "Module_SCORM"
-scorm_title = st.text_input("Titre du module SCORM", value=default_title)
-scorm_filename = st.text_input("Nom du fichier SCORM (zip)", value=re.sub(r"[^\w\-]", "_", scorm_title))
+scorm_title = st.text_input("Titre du module SCORM (sert aussi de nom de fichier ZIP)", value=default_title)
+scorm_filename = re.sub(r"[^\w\-]", "_", scorm_title)
 
 # Choix version SCORM
 st.subheader("Version SCORM")
