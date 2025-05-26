63d94ee50e7ae7c33edb43d776c0a03329d9ab8e
ColinLeP1 <154586650+ColinLeP1@users.noreply.github.com>
2025-05-26 15:06:52 +0200

    Update mp3_scorm.py

--- mp3_scorm.py
+++ mp3_scorm.py
@@ -85,8 +85,8 @@ def create_scorm_package(mp3_path, subtitle_paths, output_dir, version, scorm_ti
             shutil.copy(path, os.path.join(output_dir, filename))
 
     track_elements = "\n    ".join([
-        f'<track src="{fn}" kind="subtitles" srclang="{os.path.splitext(fn)[0].split("_")[-1]}" label="{os.path.splitext(fn)[0].split("_")[-1].capitalize()}"/>' 
-        for fn in subtitle_filenames
+    f'<track src="{fn}" kind="subtitles" srclang="{os.path.splitext(fn)[0].split("_")[-1]}" label="{os.path.splitext(fn)[0].split("_")[-1].capitalize()}" />' 
+    for fn in subtitle_filenames
     ])
 
     html_content = f'''<!DOCTYPE html>
